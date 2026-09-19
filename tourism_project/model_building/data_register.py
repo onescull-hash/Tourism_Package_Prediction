0f7f6b33abec577b82433a282a4d605fb2aec25c
onescull-hash <esemosipaul@gmail.com>
2026-09-19 11:12:04 +0100

    update tourism_project/model_building/data_register.py

--- tourism_project/model_building/data_register.py
+++ tourism_project/model_building/data_register.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-RAW_PATH = "data/tourism.csv"  # complete the code: path to the raw tourism.csv file inside the data folder
+RAW_PATH = "tourism_project/data/tourism.csv"  # complete the code: path to the raw tourism.csv file inside the data folder
 
 # Load the raw dataset
 df = pd.read_csv(RAW_PATH)
